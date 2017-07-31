8629f6916cf6c83204812dac5652315c52dd9157
Jacob Stovall <jacob.stovall@asu.edu>
2017-07-30 20:52:40 -0700

    Used Vue-CLI to generate starter project and tested a few things with it

---
+++ jacobstovallwebsite/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      message: 'Hello World'
+    }
+  }
+}
+</script>
